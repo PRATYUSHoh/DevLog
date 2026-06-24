fc1de710db379024618f94dcbe61fa35a6ab4e01
PRATYUSHoh <rishu442005@gmail.com>
2026-06-24 11:36:42 -0600

    chore: mount folder and file routes in app.js, fix import order

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const session = require('express-session');
 const passport = require('passport');
 const pgSession = require('connect-pg-simple')(session);
 const pool = require('./config/database');
-const routes = require('./routes');
 require('dotenv').config();
 require('./config/passport');
 
+const routes        = require('./routes');
 const postRoutes    = require('./routes/posts');
 const commentRoutes = require('./routes/comments');
 const fileRoutes    = require('./routes/files');
+const foldersRouter = require('./routes/folders');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -40,7 +41,7 @@ app.use('/api/posts', postRoutes);
 app.use('/api/posts/:id/comments', commentRoutes);
 app.use('/api/comments', commentRoutes);
 app.use('/api/files', fileRoutes);
-
+app.use('/api/folders', foldersRouter);
 app.use(routes);
 
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
